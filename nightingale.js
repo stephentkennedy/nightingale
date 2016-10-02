379c2ec8d4ae7256a6f186b113cae571c92e7925
stephentkennedy <stephen.thomas.kennedy@gmail.com>
2016-10-02 18:08:42 -0500

    Add files via upload
    nightingale script v0.2a

---
+++ nightingale.js
@@ -0,0 +1,189 @@
+//Nightingale v 0.2a written by Stephen T. Kennedy (C)2016
+var poem2 = "~clear~  ~lttr~ <3 ~wait~  ~wait~  ~wait~  ~wait~  ~wait~  ~clear~  ~fast~ I find myself lately, looking for solace in nostalgia. ~/lttr~ <br> ~lttr~ Hoping to remember the ~/lttr~ <i>good</i> ~lttr~ times.";
+var poem = "~clear~  ~zoom~  ~lttr~ <!DOCTYPE html> ~/lttr~ <br> ~lttr~ <html> ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ <head> ~/lttr~ <br> ~lttr~ <meta charset='utf-8' /> ~/lttr~ <br> ~lttr~ <title>Retro, A Poem.</title> ~/lttr~ <br> ~lttr~ <style> ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ ::-webkit-scrollbar { ~/lttr~ <br> ~lttr~ -webkit-appearance: none; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ ::-webkit-scrollbar:vertical { ~/lttr~ <br> ~lttr~ width: 5px; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ ::-webkit-scrollbar:horizontal { ~/lttr~ <br> ~lttr~ height: 0px; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ ::-webkit-scrollbar-thumb { ~/lttr~ <br> ~lttr~ background-color: rgba(0, 0, 0, 1); ~/lttr~ <br> ~lttr~ border-radius: 4px; ~/lttr~ <br> ~lttr~ border: 1px solid #000000; ~/lttr~ <br> ~lttr~ box-shadow: 0px 0px 2px #000000; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ ::-webkit-scrollbar-track { ~/lttr~ <br> ~lttr~ border-radius: 4px;  ~/lttr~ <br> ~lttr~ background-color: #b8b894; ~/lttr~ <br> ~lttr~ border: 1px solid #000000; ~/lttr~ <br> ~lttr~ box-shadow: 0px 0px 2px #000000; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ .numberButton { ~/lttr~ <br> ~lttr~ text-decoration: none; ~/lttr~ <br> ~lttr~ background-color: #999999; ~/lttr~ <br> ~lttr~ color: #000000; ~/lttr~ <br> ~lttr~ position: relative; ~/lttr~ <br> ~lttr~ width: 20px; ~/lttr~ <br> ~lttr~ height: 20px; ~/lttr~ <br> ~lttr~ font-size: large; ~/lttr~ <br> ~lttr~ border: line; ~/lttr~ <br> ~lttr~ float: right; ~/lttr~ <br> ~lttr~ border-radius: 8px; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ .numberButton:active { ~/lttr~ <br> ~lttr~ background-color: #FFFFFF; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ .zeroButton { ~/lttr~ <br> ~lttr~ text-decoration: none; ~/lttr~ <br> ~lttr~ background-color: #999999; ~/lttr~ <br> ~lttr~ color: #000000; ~/lttr~ <br> ~lttr~ position: relative; ~/lttr~ <br> ~lttr~ width: 50%; ~/lttr~ <br> ~lttr~ height: 15.75%; ~/lttr~ <br> ~lttr~ font-size: xx-large; ~/lttr~ <br> ~lttr~ border: line; ~/lttr~ <br> ~lttr~ float: left; ~/lttr~ <br> ~lttr~ border-radius: 8px; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ .zeroButton:active { ~/lttr~ <br> ~lttr~ background-color: #FFFFFF; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ .equalButton { ~/lttr~ <br> ~lttr~ text-decoration: none; ~/lttr~ <br> ~lttr~ background-color: #000055; ~/lttr~ <br> ~lttr~ color: #FFFFFF; ~/lttr~ <br> ~lttr~ position: relative; ~/lttr~ <br> ~lttr~ width: 75%; ~/lttr~ <br> ~lttr~ height: 15.75%; ~/lttr~ <br> ~lttr~ font-size: xx-large; ~/lttr~ <br> ~lttr~ border: line; ~/lttr~ <br> ~lttr~ float: left; ~/lttr~ <br> ~lttr~ border-radius: 8px; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ .equalButton:active { ~/lttr~ <br> ~lttr~ background-color: #000000; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ .operationsButton { ~/lttr~ <br> ~lttr~ text-decoration: none; ~/lttr~ <br> ~lttr~ background-color: #000055; ~/lttr~ <br> ~lttr~ color: #FFFFFF; ~/lttr~ <br> ~lttr~ position: relative; ~/lttr~ <br> ~lttr~ width: 25%; ~/lttr~ <br> ~lttr~ height: 15.75%; ~/lttr~ <br> ~lttr~ font-size: xx-large; ~/lttr~ <br> ~lttr~ border: line; ~/lttr~ <br> ~lttr~ float: left; ~/lttr~ <br> ~lttr~ border-radius: 8px; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ .operationsButton:active { ~/lttr~ <br> ~lttr~ background-color: #000000; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ </style> ~/lttr~ <br> ~lttr~ </head> ~/lttr~ <br> ~lttr~ <body   #000000;overflow: hidden;'> ~/lttr~ <br> ~lttr~ <div id ='container'  8px;background-color: #FFFFFF;overflow: hidden;width: 100%; height: 550px;border-style: solid;border-color: #FFFFFF;'> ~/lttr~ <br> ~lttr~ <div id='titlebar'  relative;color: #000000;text-align: center;font-size: x-large;width; 100%; height: 5%;'><i><b>Retro</b></i> - Stephen Kennedy</div> ~/lttr~ <br> ~lttr~ <div id='poem'  url('');background-repeat: no-repeat;background-position: right;background-color: #b8b894;position: relative;color: #000000;z-index: 9;text-align: right;text-shadow: 0px 0px 1px #000000;overflow: auto;font-size: x-large;border-style:inset; border-color: #FFFFFF; width: 99%; height: 90%;box-shadow: 0px 0px 5px #000000 inset'>Click<br>| <br>| <br>| <br>V ~/lttr~ <br> ~lttr~ </div> ~/lttr~ <br> ~lttr~ <button class='numberButton' ></button> ~/lttr~ <br> ~lttr~ </div> ~/lttr~ <br> ~lttr~ <javascript> ~/lttr~ <br> ~lttr~ //Nightingale v 0.2a written by Stephen T. Kennedy (C)2016 ~/lttr~ <br> ~lttr~ var poem = '~clear~ ~lttr~<3~wait~~wait~~wait~~wait~~wait~~clear~~fast~I find myself lately, looking for solace in nostalgia.~/lttr~<br>~lttr~Hoping to remember the~/lttr~<i>good</i>~lttr~times.'; ~/lttr~ <br> ~lttr~ var poem2 = ''; ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ var poemIndex = 0; ~/lttr~ <br> ~lttr~ var poemArray = []; ~/lttr~ <br> ~lttr~ var wordIndex = 0; ~/lttr~ <br> ~lttr~ var wordArray = []; ~/lttr~ <br> ~lttr~ var screen = document.getElementById('poem'); ~/lttr~ <br> ~lttr~ var letterBool = false; ~/lttr~ <br> ~lttr~ var spaceBool = true; ~/lttr~ <br> ~lttr~ var opacityInterval = 0; ~/lttr~ <br> ~lttr~ var opacity = 0; ~/lttr~ <br> ~lttr~ var riseInterval = 0; ~/lttr~ <br> ~lttr~ var margin = 720; ~/lttr~ <br> ~lttr~ var go = ''; ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ var poemLength = 0; ~/lttr~ <br> ~lttr~ var opacityStep = 0; ~/lttr~ <br> ~lttr~ var transformStep = 0; ~/lttr~ <br> ~lttr~ var sunPos = 360; ~/lttr~ <br> ~lttr~ var cloudsOp = 0; ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ var loaded = 0; ~/lttr~ <br> ~lttr~ var loadOp = 0; ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ function countOff() ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ go = setInterval(begin, 1); ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ function begin() ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ if (loadOp > 0) ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ loadOp = loadOp - .01; ~/lttr~ <br> ~lttr~ loader.style.opacity = loadOp; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (go == '') ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ clearInterval(go); ~/lttr~ <br> ~lttr~ screen.scrollTop = screen.scrollHeight; ~/lttr~ <br> ~lttr~ generateArray('poem'); ~/lttr~ <br> ~lttr~ go = setInterval(controller, 200); ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ function resize() ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ //document.getElementById('container').style.width = window.innerWidth; ~/lttr~ <br> ~lttr~ //document.getElementById('container').style.height = window.innerHeight; ~/lttr~ <br> ~lttr~ document.getElementById('container').style.width = window.screen.innerWidth; ~/lttr~ <br> ~lttr~ document.getElementById('container').style.height = window.screen.innerHeight; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ function generateArray (type) ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ if (type == 'poem') ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ poemArray = poem.split(' '); ~/lttr~ <br> ~lttr~ poemIndex = 0; ~/lttr~ <br> ~lttr~ var opacityInterval = 1 / poemArray.length; ~/lttr~ <br> ~lttr~ var riseInterval = 720 / poemArray.length; ~/lttr~ <br> ~lttr~ poemLength = poemArray.length; ~/lttr~ <br> ~lttr~ opacityStep = 1 / poemLength; ~/lttr~ <br> ~lttr~ transformStep = 720 / poemLength; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (type == 'word') ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ wordArray = poemArray[poemIndex].split(''); ~/lttr~ <br> ~lttr~ wordIndex = 0; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ screen.innerHTML = '<b>Critical Error: Improperly called generateArray()</b>'; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ function readPoem() ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ var read = poemArray[poemIndex] ~/lttr~ <br> ~lttr~ var write = true; ~/lttr~ <br> ~lttr~ if (read == '~lttr~') ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ letterBool = true; ~/lttr~ <br> ~lttr~ write = false; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (read == '~/lttr~') ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ letterBool = false; ~/lttr~ <br> ~lttr~ write = false; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (read == '~space~') ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ spaceBool = true; ~/lttr~ <br> ~lttr~ write = false; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (read == '~/space~') ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ spaceBool = false; ~/lttr~ <br> ~lttr~ write = false; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (read == '~wait~') ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ write = false; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (read == '~slow~') ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ clearInterval(go); ~/lttr~ <br> ~lttr~ go = setInterval(controller , 1000); ~/lttr~ <br> ~lttr~ write = false; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (read == '~fast~') ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ clearInterval(go); ~/lttr~ <br> ~lttr~ go = setInterval(controller , 100); ~/lttr~ <br> ~lttr~ write = false; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (read == '~normal~') ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ clearInterval(go); ~/lttr~ <br> ~lttr~ go = setInterval(controller , 200); ~/lttr~ <br> ~lttr~ write = false; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (read == '~clear~') ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ screen.innerHTML = ''; ~/lttr~ <br> ~lttr~ write = false; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ if (write == true) ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ if (letterBool == true) ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ generateArray('word'); ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ writeScreen(); ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ poemIndex = poemIndex + 1; ~/lttr~ <br> ~lttr~ if (letterBool == true) ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ generateArray('word') ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ function writeScreen() ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ if (letterBool == true && wordIndex <= wordArray.length - 1) ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ screen.innerHTML += wordArray[wordIndex]; ~/lttr~ <br> ~lttr~ wordIndex = wordIndex + 1; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (letterBool == true && wordIndex > wordArray.length - 1) ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ screen.innerHTML += ' '; ~/lttr~ <br> ~lttr~ poemIndex = poemIndex + 1; ~/lttr~ <br> ~lttr~ readPoem(); ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (letterBool == false && spaceBool == false && poemIndex <=  poemArray.length - 1) ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ screen.innerHTML += poemArray[poemIndex]; ~/lttr~ <br> ~lttr~ poemIndex = poemIndex + 1; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (letterBool == false && spaceBool == true && poemIndex <= poemArray.length - 1) ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ screen.innerHTML+= poemArray[poemIndex] + ' '; ~/lttr~ <br> ~lttr~ poemIndex = poemIndex + 1; ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else if (poemIndex > poemArray.length - 1) ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ clearInterval(go); ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ function controller() ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ screen.scrollTop = screen.scrollHeight; ~/lttr~ <br> ~lttr~ if (letterBool == true) ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ writeScreen(); ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ else ~/lttr~ <br> ~lttr~ { ~/lttr~ <br> ~lttr~ readPoem(); ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~ } ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ resize(); ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ </javascript> ~/lttr~ <br> ~lttr~ </body> ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ </html> ~/lttr~  ~fast~  ~clear~  ~lttr~ <3 ~wait~  ~wait~  ~wait~  ~wait~  ~wait~  ~clear~ I find myself lately, looking for solace in nostalgia. ~/lttr~ <br> ~lttr~ Hoping to remember the ~/lttr~ <i>good</i> ~lttr~ times. ~/lttr~ <br> ~lttr~ It takes aprox. 48 and one half seconds to print the code that makes this work, ~/lttr~ <br> ~lttr~ Almost 11,945 characters, ~/lttr~ <br> ~lttr~ give or take a couple hundred for the tags that drive Nightingale. ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ I don't do much in the way of self-love. ~/lttr~ <br> ~lttr~ but ~/lttr~ <i>these</i> ~lttr~ are the good times. ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ That's 11,945 characters of hand-written code. ~/lttr~ <br> ~lttr~ That's 11,945 things I couldn't do a year ago. ~/lttr~ <br> ~lttr~   ~/lttr~ <br> ~lttr~ So why then, ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ do I feel useless. ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ I can feel depression gnawing-- ~/lttr~ <br> ~lttr~ at those 11,945 reasons to be proud ~/lttr~ <br> ~lttr~ and I can't help but wonder 'why?' ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ 48 and a half seconds to distill three years of self-education ~/lttr~ <br> ~lttr~ seems a little ridiculous. ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ I mean, ~wait~  ~wait~ I used to dream about the day-- ~/lttr~ <br> ~lttr~ I could bring text to life ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ that I could do something I'd never seen anyone else do. ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ And it's been a long time since I was using dry erase on dorm windows. ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ And it's been a long time. ~/lttr~ <br> ~lttr~ And I still don't think these words, ~/lttr~ <br> ~lttr~ those words, ~/lttr~ <br> ~lttr~ do as much for anyone as they do me. ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ and I wonder, ~/lttr~ <br> ~lttr~ I wonder. ~/lttr~ <br> ~lttr~ Wonder where the wonder's gone. ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ And it probably says something, ~/lttr~ <br> ~lttr~ about me, ~/lttr~ <br> ~lttr~ that I'm using all this knowledge ~/lttr~ <br> ~lttr~ to bring half-life to a disparaged form. ~/lttr~ <br> ~lttr~  ~/lttr~ <br> ~lttr~ But here we are 11,945 characters later. ~stop~";
+
+var poemIndex = 0;
+var poemArray = [];
+var wordIndex = 0;
+var wordArray = [];
+var screen = document.getElementById("poem");
+var letterBool = false;
+var spaceBool = true;
+var opacityInterval = 0;
+var opacity = 0;
+var riseInterval = 0;
+var margin = 720;
+var go = "";
+
+var poemLength = 0;
+var opacityStep = 0;
+var transformStep = 0;
+var sunPos = 360
+var cloudsOp = 0;
+
+
+var loaded = 0;
+var loadOp = 0;
+
+function begin()
+{
+	if (loadOp > 0)
+	{
+		loadOp = loadOp - .01;
+		loader.style.opacity = loadOp;
+	}
+	else if (go == "")
+	{
+		clearInterval(go);
+		screen.scrollTop = screen.scrollHeight;
+		generateArray("poem");
+		go = setInterval(controller, 200);
+	}
+}
+
+function generateArray (type)
+{
+	if (type == "poem")
+	{
+		poemArray = poem.split(" ");
+		poemIndex = 0;
+		var opacityInterval = 1 / poemArray.length;
+		var riseInterval = 720 / poemArray.length;
+		poemLength = poemArray.length;
+		opacityStep = 1 / poemLength;
+		transformStep = 720 / poemLength;
+	}
+	else if (type == "word")
+	{
+		wordArray = poemArray[poemIndex].split("");
+		wordIndex = 0;
+	}
+	else
+	{
+		screen.innerHTML = "<b>Critical Error: Improperly called generateArray()</b>";
+	}
+}
+
+
+
+function readPoem()
+{
+	var read = poemArray[poemIndex]
+	var write = true;
+	if (read == "~lttr~")
+	{
+		letterBool = true;
+		write = false;
+	}
+	else if (read == "~/lttr~")
+	{
+		letterBool = false;
+		write = false;
+	}
+	else if (read == "~space~")
+	{
+		spaceBool = true;
+		write = false;
+	}
+	else if (read == "~/space~")
+	{
+		spaceBool = false;
+		write = false;
+	}
+	else if (read == "~wait~")
+	{
+		write = false;
+	}
+	else if (read == "~slow~")
+	{
+		clearInterval(go);
+		go = setInterval(controller , 1000);
+		write = false;
+	}
+	else if (read == "~fast~")
+	{
+		clearInterval(go);
+		go = setInterval(controller , 100);
+		write = false;
+	}
+	else if (read == "~zoom~")
+	{
+		clearInterval(go);
+		go = setInterval(controller , 1);
+		write = false;
+	}
+	else if (read == "~normal~")
+	{
+		clearInterval(go);
+		go = setInterval(controller , 200);
+		write = false;
+	}
+	else if (read == "~clear~")
+	{
+		screen.innerHTML = "";
+		write = false;
+	}
+	else if (read == "~stop~")
+	{
+		write = false;
+		clearInterval(go);
+	}
+	if (write == true)
+	{
+		if (letterBool == true)
+		{
+			generateArray("word");
+		}
+		writeScreen();
+	}
+	else
+	{
+		poemIndex = poemIndex + 1;
+		if (letterBool == true)
+		{
+			generateArray("word")
+		}
+	}
+}
+
+function writeScreen()
+{
+	if (letterBool == true && wordIndex <= wordArray.length - 1)
+	{
+		screen.innerHTML += wordArray[wordIndex];
+		wordIndex = wordIndex + 1;
+	}
+	else if (letterBool == true && wordIndex > wordArray.length - 1)
+	{
+		screen.innerHTML += " ";
+		poemIndex = poemIndex + 1;
+		readPoem();
+	}
+	else if (letterBool == false && spaceBool == false && poemIndex <= poemArray.length - 1)
+	{
+		screen.innerHTML += poemArray[poemIndex];
+		poemIndex = poemIndex + 1;
+	}
+	else if (letterBool == false && spaceBool == true && poemIndex <= poemArray.length - 1)
+	{
+		screen.innerHTML += poemArray[poemIndex] + " ";
+		poemIndex = poemIndex + 1;
+	}
+	else if (poemIndex > poemArray.length - 1)
+	{
+		clearInterval(go);
+	}
+}
+
+function controller()
+{
+	screen.scrollTop = screen.scrollHeight;
+	if (letterBool == true)
+	{
+		writeScreen();
+	}
+	else
+	{
+		readPoem();
+	}
+}
